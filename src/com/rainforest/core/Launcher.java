0f7b43ec38e376f1bb24de2e264644d3137860a8
viceng01 <viceng01@ucm.es>
2022-03-01 14:22:20 +0100

    Performed requested changes

--- src/com/rainforest/core/Launcher.java
+++ src/com/rainforest/core/Launcher.java
@@ -3,7 +3,7 @@ package com.rainforest.core;
 public class Launcher {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!"); 
+		System.out.println("Hello, world! This was changed with Git!");
 	}
 
 }
