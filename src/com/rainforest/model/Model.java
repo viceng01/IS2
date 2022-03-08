4c7865268a9f1ada2e4d3a2bbe5e2c3e73fe8816
viceng01 <viceng01@ucm.es>
2022-03-08 14:12:06 +0100

    Added Sellers to model

--- src/com/rainforest/model/Model.java
+++ src/com/rainforest/model/Model.java
@@ -1,5 +1,17 @@
 package com.rainforest.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.rainforest.model.seller.Seller;
+
 public class Model {
 
+	private Set<Seller> sellerSet;
+
+	public Model() {
+		sellerSet = new HashSet<>();
+	}
+	
+	
 }
