19d68f838e8f9c7bf338085567f78585513d4953
viceng01 <viceng01@ucm.es>
2022-03-08 14:03:21 +0100

    Added info

--- src/com/rainforest/model/seller/SellerInfo.java
+++ src/com/rainforest/model/seller/SellerInfo.java
@@ -6,5 +6,23 @@ public class SellerInfo {
 
 	private String name;
 	private GUID sellerID;
-	
+	private String email;
+
+	public SellerInfo(String name, GUID sellerID, String email) {
+		this.name = name;
+		this.sellerID = sellerID;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public GUID getSellerID() {
+		return sellerID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
 }
